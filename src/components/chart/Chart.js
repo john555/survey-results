1cd65c6ad8b760b91e75011484ab5d922727fdd1
John O. Paul <oyomthojohnpaul@gmail.com>
2018-08-06 17:19:27 +0300

    Add labels to chart

--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import * as d3 from 'd3';
 
 class Chart extends Component {
-  static swatch = ['#7AA532', '#EAD259', '#385E89', '#526561', '#97B79E', '#C7E0CC', '#A9767E', '#D19B8C', '#EDE4DA'];
+  static swatch = ['#7AA532', '#385E89', '#526561', '#A9767E', '#D19B8C', '#EDE4DA', '#EAD259', '#97B79E'];
 
   static defaultProps = {
     swatch: Chart.swatch,
@@ -20,10 +20,42 @@ class Chart extends Component {
   constructor(props) {
     super(props);
     this.figure = React.createRef();
+    this.tooltip = React.createRef();
+
+    this.state = {
+      tooltipText: '',
+    }
   }
 
   componentDidMount() {
     this.drawChart()
+  }
+
+  sortOrder(a, b) {
+    return a.value - b.value;
+  }
+
+  onChartMouseOver =(data, d) => {
+    if (this.state.tooltipText.length === 0) {
+      this.setState({
+        tooltipText: `${data.data.value} Response${data.data.value > 1 ? 's' : ''}`
+      });
+    }
+
+    const tooltipBox = this.tooltip.current.getBoundingClientRect();
+    const x = d3.event.clientX - (tooltipBox.width / 2) ;
+    const y =  d3.event.clientY - tooltipBox.height - 20;
+
+    this.tooltip.current.style['left'] = `${x}px`;
+    this.tooltip.current.style['top'] = `${y}px`;
+
+    
+
+    
+  }
+
+  onChartMouseLeave = () => {
+    this.setState({tooltipText: ''})
   }
 
   drawChart() {
@@ -46,7 +78,7 @@ class Chart extends Component {
 
 
     const pie = d3.pie()
-    .sort((a, b) => a - b) // ascending order
+    .sort(this.sortOrder)
     .value((d) => {
       return d.value;
     });
@@ -61,11 +93,13 @@ class Chart extends Component {
       .data(pie(this.props.data))
       .enter()
       .append('g')
-      .attr('class', 'chart__arc');
+      .attr('class', 'chart__arc')
+      .on('mousemove', this.onChartMouseOver)
+      .on('mouseleave', this.onChartMouseLeave);;
 
     g.append('path')
     .attr('d', arc)
-    .style('fill', (d, i) => this.props.swatch[i]);
+    .style('fill', (d, i) => this.props.swatch[i])
 
     const labelArc = d3.arc()
       .outerRadius(radius* 1.5)
@@ -80,16 +114,44 @@ class Chart extends Component {
     .text((d) => {
       const angle = d.endAngle - d.startAngle;
       const percentage = Math.round((angle * 100)/(2 * Math.PI));
-      return `${percentage}% - ${d.data.label}`;
+      return `${percentage}%`;
     });
 
   }
 
+  renderLabels() {
+    return this.props.data.map((d, i) => (
+      <div key={Math.random()} className="chart__label">
+        <span style={{backgroundColor: this.props.swatch[i]}} className="chart__color"></span>
+        <span className="chart__labelText">
+          {`${d.label}`}
+        </span>
+      </div>
+    ));
+  }
+
+  renderTooltip() {
+    const classNameStr = `chart__tooltip ${(this.state.tooltipText.length < 1) ? 'chart__tooltip--hidden':''}`;
+    return (
+      <span className={classNameStr} ref={this.tooltip}>
+        {this.state.tooltipText}
+      </span>
+    )
+  }
+
   render() {
     return (
-      <figure className="chart" ref={this.figure}>
-        {/* chart */}
-      </figure>  
+      <Fragment> 
+        <figure className="chart" ref={this.figure}>
+          {/* chart */}
+        </figure> 
+        <div className="chart__labels">
+          {
+            this.renderLabels()
+          }
+        </div>
+        {this.renderTooltip()}
+      </Fragment> 
      );
   }
 }
